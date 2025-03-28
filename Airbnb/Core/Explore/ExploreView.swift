8da6cdce9e229b7ff747d54120520a334f94e34c
Forrest-Arnold <forkalarn@gmail.com>
2025-03-28 16:01:52 -0700

    working on detail page

--- Airbnb/Core/Explore/ExploreView.swift
+++ Airbnb/Core/Explore/ExploreView.swift
@@ -11,11 +11,20 @@ struct ExploreView: View {
     var body: some View {
         NavigationStack {
             ScrollView {
+                SearchAndFilterBar()
+                
                 LazyVStack(spacing: 32) {
                     ForEach(0 ... 10, id: \.self) { listing in
-                        ListingItemView()
+                        NavigationLink(value: listing) {
+                            ListingItemView()
+                                .frame(height: 400)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                        }
                     }
                 }
+            }
+            .navigationDestination(for: Int.self) { listing in
+                Text("Listing detail view...")
             }
         }
     }
