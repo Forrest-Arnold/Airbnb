b20000dbd33bca5b64e18904637a9e62bcee07f0
Forrest-Arnold <forkalarn@gmail.com>
2025-03-28 15:16:43 -0700

    Initial Commit

---
+++ AirbnbTests/AirbnbTests.swift
@@ -0,0 +1,16 @@
+//
+//  AirbnbTests.swift
+//  AirbnbTests
+//
+//  Created by Forrest Kalani Arnold on 3/28/25.
+//
+
+import Testing
+
+struct AirbnbTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
